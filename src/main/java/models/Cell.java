857d4508118634f66764bc67af5c88d6e87b176a
Brignone Federico (Consulente) <federico.brignone@consulenti.axerve.com>
2023-03-09 17:07:10 +0100

    class Cell

--- src/main/java/models/Cell.java
+++ src/main/java/models/Cell.java
@@ -1,6 +1,27 @@
 package models;
 
 public class Cell {
-    public short score;
-    public short snakeId;
+    private short score;
+    private short snakeId;
+
+    public Cell(short score, short snakeId) {
+        this.score = score;
+        this.snakeId = snakeId;
+    }
+
+    public short getScore() {
+        return score;
+    }
+
+    public void setScore(short score) {
+        this.score = score;
+    }
+
+    public short getSnakeId() {
+        return snakeId;
+    }
+
+    public void setSnakeId(short snakeId) {
+        this.snakeId = snakeId;
+    }
 }
